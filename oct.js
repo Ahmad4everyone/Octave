6f5350064328a82918facd0a4b615e45e102eb7b
ahmad4everyone <ahmadmut24@gmail.com>
2023-11-20 03:22:14 -0800

    octave first commit

---
+++ oct.js
@@ -0,0 +1,8 @@
+var close = document.querySelector(".ion")
+var open = document.querySelector(".menu")
+close.addEventListener("click",()=>{
+    menu.style.visibility = "hidden"
+})
+open.addEventListener("click",()=>{
+    menu.style.visibily = "visible"
+})
